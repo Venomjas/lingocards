fbe3742f9ce9978bc95278613bda45713c43c110
Venomjas <81695632+Venomjas@users.noreply.github.com>
2023-03-26 18:29:22 +0300

    add title

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import logo from "./logo.svg";
 import "./App.css";
 import "./Cards/Cards";
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <h1>lingocards</h1>
+  </div>;
 }
 
 export default App;
